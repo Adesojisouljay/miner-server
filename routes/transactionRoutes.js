0b443581b249f4cf7b57e6738adb734534b7f7a0
Adesojisouljay <adesojiisrael@gmail.com>
2024-08-23 14:09:05 +0100

    calc fee

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUserTransactions, getAllTransactions, sellAsset, buyAsset, transferNairaBalance } from "../controllers/transaction.js";
+import { getUserTransactions, getAllTransactions, sellAsset, buyAsset, calculateTransaction, transferNairaBalance } from "../controllers/transaction.js";
 import { authMiddleware, isAdminMiddleware } from '../middleware/authMiddleWare.js';
 
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/', authMiddleware, getUserTransactions);
 router.post('/buy', authMiddleware, buyAsset);
 router.post('/sell', authMiddleware, sellAsset);
+router.get('/fee', authMiddleware, calculateTransaction);
 router.get('/all', getAllTransactions);
 router.post('/fiat-transfer', authMiddleware, transferNairaBalance);
 
